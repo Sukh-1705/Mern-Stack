836df9cf770fef2ef087669153c65d160c924610
Sukh-1705 <bansal.sukdev@gmail.com>
2026-07-22 18:39:14 +0530

    Initial commit

---
+++ phase_01_HTML_CSS_JS/day_07/functions.js
@@ -0,0 +1,17 @@
+let checkNationality=function(country="India") {
+    return 'This Person has ${country} nationality'
+}
+let res = checkNationality("japan")
+console.log(res)
+
+
+let x = (a) => a % 2==0
+console.log(x(7))
+
+let res = (a,b) => {
+    let c=10
+    let num=(a+b)-c
+    return num
+}
+console.log(res(20,5))
+
