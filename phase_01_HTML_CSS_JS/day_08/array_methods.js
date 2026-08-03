977685832e116c424588d66450b5b1de2910754c
Sukh-1705 <bansal.sukdev@gmail.com>
2026-08-03 18:02:40 +0530

    Initial commit

--- phase_01_HTML_CSS_JS/day_08/array_methods.js
+++ phase_01_HTML_CSS_JS/day_08/array_methods.js
@@ -1,52 +1,71 @@
-// // // //foreach, map, filter, reduce, sort ---> hof, array methods
+// foreach, map, filter, reduce, sort ---> hof, array methods
 
-// // // // let arr=["Javascript", "HTML", "CSS",];
-// // // // console.log(arr[1]);
-// // // // //length
-// // // // console.log(arr.length);
+// let arr=["Javascript","HTML","CSS"]
+// // console.log(arr[1])
 
-// // // // let a = ["HTML", "CSS", "JS", "React",];
-// // // // let s = a.toString();
-// // // // console.log(s);
+// // length
+// console.log(arr.length)
 
-// // // let a = ["HTML", "CSS", "JS", "React",];
-// // // //if you want to combine the array elements with a specific separator, you can use the join method
-// // // console.log(a.join(" | "));
+// let a  = ["HTML", "CSS", "JS", "React"];
+// let s = a.toString();
+// console.log(s);
 
-// // let a1 = [11, 12, 13];
-// // let a2 = [14, 15, 16];
-// // let a3 = [17, 18, 19];
-// // let newArr = a1.concat(a2, a3); // to combine multiple arrays into a single array
+// let a = ["HTML", "CSS", "JS", "React"]; 
+// // if you want to combine array of string with specified seperator
+// // then we can use join method
+// console.log(a.join('|'));
+
+// let a1 = [11, 12, 13];
+// let a2 = [14, 15, 16];
+// let a3 = [17, 18, 19];
+// // let newArr = a1.concat(a2, a3); // to combine multiple arrays
 // // console.log(newArr);
 
 // // let a = [10, 20, 30, 40, 50];
-// // a.push(60); // adds an element to the end of the array
+// // a.push(60);
+// // a.push(70, 80, 90); // will add element in last
 // // console.log(a);
 
-// //unshift
+// // unshift
 
-// // let arr = [30, 50, 70];
-// // arr.unshift(10, 20); // adds elements to the beginning of the array
-// // console.log(arr);
+// let arr=[30,50,70] 
+// arr.unshift(10,15)  // will add elements in begining 
+// console.log(arr)
 
-// //shift delete the first element of the array
-// let a = [10, 20, 30, 40, 50];
-// a.shift(); 
+// shift --> will remove first element
+// pop --> will remove last element
+// splice --> 
+// let a = [20, 30, 40, 50];
+// a.splice(1,2) // a,b --> a=>(index) , b=> number of elements to delete
+// // [20,50]
+// a.splice(1,0,90,100,110) // c--> Elements 
+// console.log(a)
+
+// slice
+
+// let arr=[3,5,7,9,11,13]
+// let res=arr.slice(2,5)
+// console.log(res)
+
+
+// let a = [1, 2, 3, 4, 5];
+// a.reverse();
 // console.log(a);
-// //pop will remove the last element
-// //spice --> removes elements from an array and (optionally) replaces them
-// let a = [10, 20, 30, 40, 50];
-// a.splice(2, 1); // removes 1 element at index 2
-// // [20, 50]
-// console.log(a);
 
-// let a = [10, 20, 30, 40, 50];
-// let res= a.slice(2,5)
-// console.log(res); // [30, 40, 50]  slice will return a new array with the selected elements
+// let ar=["Html","Js","Css"]
+// ar.shift() 
+// console.log(ar) // ["JS","Css"]
 
-let a = [10, 20, 30, 40, 50];
-a.reverse(); // reverses the order of the elements in the array
-console.log(a); 
+// let ar=["Html","Js","Css"]
+// ar.unshift("Java")
+// console.log(ar) //["Java","Html","Js","Css"]
 
 
+// let ar=["Html","Js","Css"]
+// // ar.splice(1,2) 
+// ar.splice(1,2,"Maths","AI")
+// console.log(ar) // ["Html","Maths","AI"]
+
+// let ar=["Html","Js","Css","Math","AI"]
+// console.log(ar.slice(1,3)) //["JS","Css"] 
 
